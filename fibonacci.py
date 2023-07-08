64abe1af26847f750479809d501de0b60ccd4199
haimf <haimfeld@gmail.com>
2023-07-09 00:28:50 +0300

    replaced i with underscore and beautify file

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 def fibnum():
-    N=input("How many Fibonacci sequence numbers do you want? ")
-    a,b=0,1
-    for i in range(int(N)):
-        a,b=b,a+b
-        print (a)
+    N = input("How many Fibonacci sequence numbers do you want? ")
+    a, b = 0, 1
+    for _ in range(int(N)):
+        a, b = b, a + b
+        print(a)
+
+
 fibnum()
